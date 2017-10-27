a172cd2a7b98f887bd8aaceb7d7149fc474cf447
Bos Mats <mats.bos@immalle.eu>
2017-10-27 10:47:02 +0200

    Add project files.

---
+++ MyFile/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
