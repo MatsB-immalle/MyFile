b4fb8e35d2c4d0d63c0f307be2d4afb809b33fd0
Mats Bos <mats.bos@immalle.be>
2017-10-27 11:10:25 +0200

    save 1

--- MyFile/Program.cs
+++ MyFile/Program.cs
@@ -9,6 +9,26 @@ namespace MyFile
     {
         static void Main(string[] args)
         {
+            if( args.Length == 0)
+            {
+                Console.WriteLine("geef een argument");
+            }
+            else
+            {
+                switch (args[0])
+                {
+                    case "-f":
+
+                        break;
+                    case "-c":
+                        break;
+                    default:
+                        Console.WriteLine("probeer eens -c of -f");
+                        break;
+
+                }
+               
+            }
         }
     }
 }
